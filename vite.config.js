a9f245a43e536d176ffc4d2c4de2052915c281cf
ozdemirhazal <shazalozdemir@gmail.com>
2023-11-09 13:29:07 +0100

    added mime types on vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,49 @@ export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
     base: "/",
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            // Dosya uzantısına göre MIME türlerini ayarlayın
+            if (assetInfo.name.endsWith(".html")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".js")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".css")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".json")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".svg")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".png")) {
+              return `assets/[name][ext]`;
+            } else if (
+              assetInfo.name.endsWith(".jpg") ||
+              assetInfo.name.endsWith(".jpeg")
+            ) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".gif")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".ico")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".woff")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".woff2")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".eot")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".ttf")) {
+              return `assets/[name][ext]`;
+            } else if (assetInfo.name.endsWith(".otf")) {
+              return `assets/[name][ext]`;
+            } else {
+              return `assets/[name]-[hash][ext]`; // Diğer dosyalar için varsayılan ayar
+            }
+          },
+        },
+      },
+    },
   };
 
   if (command !== "serve") {
