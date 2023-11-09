91a6d2b77e2cb5a6d2731d3f3296bc08c2103cca
ozdemirhazal <shazalozdemir@gmail.com>
2023-11-09 13:33:24 +0100

    modified jsx on vite config

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: (assetInfo) => {
-            // Dosya uzantısına göre MIME türlerini ayarlayın
-            if (assetInfo.name.endsWith(".html")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".js")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".css")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".json")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".svg")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".png")) {
-              return `assets/[name][ext]`;
-            } else if (
-              assetInfo.name.endsWith(".jpg") ||
-              assetInfo.name.endsWith(".jpeg")
-            ) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".gif")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".ico")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".woff")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".woff2")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".eot")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".ttf")) {
-              return `assets/[name][ext]`;
-            } else if (assetInfo.name.endsWith(".otf")) {
-              return `assets/[name][ext]`;
-            } else {
-              return `assets/[name]-[hash][ext]`; // Diğer dosyalar için varsayılan ayar
-            }
-          },
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // .jsx uzantılı dosyalar için "text/jsx" MIME türünü ayarlayın
+          if (assetInfo.name.endsWith(".jsx")) {
+            return `assets/[name][ext]:text/jsx`;
+          }
+          // Diğer dosya türleri için varsayılan ayarları kullanın
+          return `assets/[name]-[hash][ext]`;
         },
       },
     },
-  };
-
-  if (command !== "serve") {
-    config.base = "/";
-  }
-
-  return config;
+  },
 });
