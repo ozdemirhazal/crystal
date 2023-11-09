e0cbb40e603a489fff22766710825320cfe60c8b
ozdemirhazal <shazalozdemir@gmail.com>
2023-11-09 04:24:28 +0100

    changed jsx

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+import "./Body.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  React.createElement(React.StrictMode, null, React.createElement(App, null))
+);
