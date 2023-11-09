02f5244df14cd9b5f1c48ba5d43c1f1b2c561160
ozdemirhazal <shazalozdemir@gmail.com>
2023-11-09 13:17:07 +0100

    Revert "changed jsx"
    This reverts commit e0cbb40e603a489fff22766710825320cfe60c8b.

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.js";
-import "./Body.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  React.createElement(React.StrictMode, null, React.createElement(App, null))
-);
